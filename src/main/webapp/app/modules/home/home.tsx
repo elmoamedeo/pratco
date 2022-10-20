9062eed45b2424ed9703795ac7c4d4eb952e9968
rafa-schmitz <rafaelsschmitz98@gmail.com>
2022-10-20 02:22:37 -0300

    landing page layout finished

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -21,7 +21,7 @@ export const Home = () => {
         <Col md="2" className="second-col-container">
           <div className="second-col-wrapper">
             <span className="second-col-content">
-              Coma bem, pagando pouco <span className="special-content">e de quebra,</span> ajude o meio ambiente
+              Coma bem, pagando pouco <span className="special-content">e de quebra</span>, ajude o meio ambiente
             </span>
           </div>
 
@@ -33,7 +33,9 @@ export const Home = () => {
             </Col>
 
             <Col className="buttons-col buttons-separator">
-              <span>ou</span>
+              <span>
+                <strong>ou</strong>
+              </span>
             </Col>
             <Col className="buttons-col">
               <Link to="/account/register" className="btn register-btn">
@@ -58,8 +60,40 @@ export const Home = () => {
         </span>
       </Row>
 
-      <Row>
+      <Row className="partners-row">
         <PartnersList />
+      </Row>
+
+      <div className="line" />
+
+      <Row className="about-us-row">
+        <Col className="about-us-text">
+          <h1 className="about-us-content-title">
+            <strong>
+              Ajude a combater o <span className="special-content">desperdício</span> alimentar
+            </strong>
+          </h1>
+          <p className="about-us-content">
+            Estima-se que 931 milhões de toneladas de alimentos, ou 17% do total de alimentos disponíveis aos consumidores em 2019, foram
+            para o lixo de residências, varejo, restaurantes e outros serviços alimentares. É o que aponta uma nova pesquisa da ONU que visa
+            apoiar os esforços globais para reduzir pela metade o desperdício de alimentos até 2030.
+          </p>
+          <p className="about-us-content">
+            Nossa solução visa conectar empresas com ONGs/pessoas para comercializar produtos próximos ao vencimento por um melhor preço.
+          </p>
+          <p className="about-us-content">
+            Evitando o desperdício de alimentos e levando as empresas evitarem prejuízos financeiros e permitindo que os compradores possam
+            adquirir tais produtos com preço reduzido.
+          </p>
+        </Col>
+
+        <Col className="about-us-image">
+          <img width="559px" height="559px" src="./content/images/sobre-nos.svg" alt="Sem desperdícios" />
+        </Col>
+
+        <Row>
+          <button className="join-us-btn">Faça parte desta causa</button>
+        </Row>
       </Row>
     </>
   );
